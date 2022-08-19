a8613817e128bd584d88918dfb9ea604bf415ba9
tobiakinlade <tobi.akinlade@yahoo.com>
2022-08-19 16:42:06 +0100

    about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import { useState } from 'react'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
 
 export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -19,13 +21,28 @@ export default function App() {
     setFeedback([newFeedback, ...feedback])
   }
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList handleDelete={deleteFeedback} feedback={feedback} />
+        <Routes>
+          <Route
+            path='/'
+            exact
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  handleDelete={deleteFeedback}
+                  feedback={feedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
@@ -5,13 +6,30 @@ export default function List({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
+
   return (
     <div className='feedback-list'>
-      {feedback.map((item, index) => (
-        <FeedbackItem key={index} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={index} item={item} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item, index) => (
+  //       <FeedbackItem key={index} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 List.propTypes = {
